a993f2da80103254125f9d4991ee62cf2315c1ff
ASUS <raminhm605@gmail.com>
2022-05-11 11:01:37 +0430

    initial tesla

---
+++ app/src/main/java/ir/hm/neumorphism/MainActivity.kt
@@ -0,0 +1,15 @@
+package ir.hm.neumorphism
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, MainActivity2::class.java)
+        startActivity(intent)
+    }
+}
