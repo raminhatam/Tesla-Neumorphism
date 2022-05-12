3b36980295481e3a860353be5660d71d315814f2
ASUS <raminhm605@gmail.com>
2022-05-12 09:45:37 +0430

    initial page 2

--- app/src/main/java/ir/hm/neumorphism/MainActivity.kt
+++ app/src/main/java/ir/hm/neumorphism/MainActivity.kt
@@ -3,13 +3,23 @@ package ir.hm.neumorphism
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ir.hm.neumorphism.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val intent = Intent(this, MainActivity2::class.java)
-        startActivity(intent)
+
+        binding.btnGoToActivity2.setOnClickListener {
+
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+
+        }
+
+
     }
 }
--- app/src/main/java/ir/hm/neumorphism/MainActivity2.kt
+++ app/src/main/java/ir/hm/neumorphism/MainActivity2.kt
@@ -2,11 +2,101 @@ package ir.hm.neumorphism
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import ir.hm.neumorphism.databinding.ActivityMain2Binding
+import soup.neumorphism.ShapeType
 
 class MainActivity2 : AppCompatActivity() {
+    lateinit var binding:ActivityMain2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.neumorphImageView.setOnClickListener {
+
+            binding.neumorphImageView.setShapeType(ShapeType.PRESSED)
+
+            binding.neumorphImageView.setColorFilter(
+                ContextCompat.getColor( this, R.color.blue_light ), android.graphics.PorterDuff.Mode.SRC_IN    )
+
+            binding.neumorphImageView2.setShapeType(ShapeType.FLAT)
+            binding.neumorphImageView2.setColorFilter(
+                ContextCompat.getColor( this, R.color.text_gray ), android.graphics.PorterDuff.Mode.SRC_IN    )
+
+            binding.neumorphImageView3.setShapeType(ShapeType.FLAT)
+            binding.neumorphImageView3.setColorFilter(
+                ContextCompat.getColor( this, R.color.text_gray ), android.graphics.PorterDuff.Mode.SRC_IN    )
+
+            binding.neumorphImageView4.setShapeType(ShapeType.FLAT)
+            binding.neumorphImageView4.setColorFilter(
+                ContextCompat.getColor( this, R.color.text_gray ), android.graphics.PorterDuff.Mode.SRC_IN    )
+
+        }
+
+        binding.neumorphImageView2.setOnClickListener {
+
+            binding.neumorphImageView2.setShapeType(ShapeType.PRESSED)
+
+            binding.neumorphImageView2.setColorFilter(
+                ContextCompat.getColor( this, R.color.blue_light ), android.graphics.PorterDuff.Mode.SRC_IN    )
+
+            binding.neumorphImageView.setShapeType(ShapeType.FLAT)
+            binding.neumorphImageView.setColorFilter(
+                ContextCompat.getColor( this, R.color.text_gray ), android.graphics.PorterDuff.Mode.SRC_IN    )
+
+            binding.neumorphImageView3.setShapeType(ShapeType.FLAT)
+            binding.neumorphImageView3.setColorFilter(
+                ContextCompat.getColor( this, R.color.text_gray ), android.graphics.PorterDuff.Mode.SRC_IN    )
+
+            binding.neumorphImageView4.setShapeType(ShapeType.FLAT)
+            binding.neumorphImageView4.setColorFilter(
+                ContextCompat.getColor( this, R.color.text_gray ), android.graphics.PorterDuff.Mode.SRC_IN    )
+
+        }
+
+        binding.neumorphImageView3.setOnClickListener {
+
+            binding.neumorphImageView3.setShapeType(ShapeType.PRESSED)
+
+            binding.neumorphImageView3.setColorFilter(
+                ContextCompat.getColor( this, R.color.blue_light ), android.graphics.PorterDuff.Mode.SRC_IN    )
+
+            binding.neumorphImageView2.setShapeType(ShapeType.FLAT)
+            binding.neumorphImageView2.setColorFilter(
+                ContextCompat.getColor( this, R.color.text_gray ), android.graphics.PorterDuff.Mode.SRC_IN    )
+
+            binding.neumorphImageView.setShapeType(ShapeType.FLAT)
+            binding.neumorphImageView.setColorFilter(
+                ContextCompat.getColor( this, R.color.text_gray ), android.graphics.PorterDuff.Mode.SRC_IN    )
+
+            binding.neumorphImageView4.setShapeType(ShapeType.FLAT)
+            binding.neumorphImageView4.setColorFilter(
+                ContextCompat.getColor( this, R.color.text_gray ), android.graphics.PorterDuff.Mode.SRC_IN    )
+
+        }
+
+        binding.neumorphImageView4.setOnClickListener {
+
+            binding.neumorphImageView4.setShapeType(ShapeType.PRESSED)
+
+            binding.neumorphImageView4.setColorFilter(
+                ContextCompat.getColor( this, R.color.blue_light ), android.graphics.PorterDuff.Mode.SRC_IN    )
+
+            binding.neumorphImageView2.setShapeType(ShapeType.FLAT)
+            binding.neumorphImageView2.setColorFilter(
+                ContextCompat.getColor( this, R.color.text_gray ), android.graphics.PorterDuff.Mode.SRC_IN    )
+
+            binding.neumorphImageView3.setShapeType(ShapeType.FLAT)
+            binding.neumorphImageView3.setColorFilter(
+                ContextCompat.getColor( this, R.color.text_gray ), android.graphics.PorterDuff.Mode.SRC_IN    )
+
+            binding.neumorphImageView.setShapeType(ShapeType.FLAT)
+            binding.neumorphImageView.setColorFilter(
+                ContextCompat.getColor( this, R.color.text_gray ), android.graphics.PorterDuff.Mode.SRC_IN    )
+
+        }
 
 
     }
